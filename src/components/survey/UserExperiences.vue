00f8a7c7689c94c85dd8dd1d3c1337c646a716dd
dejotb <dejotb@gmail.com>
2022-11-21 17:48:34 +0100

    added fetch API post and get

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -3,9 +3,15 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
       </div>
-      <ul>
+      <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!isLoading && (!results || results.length === 0)">
+        No stored experiences found. Start adding some results firs.
+      </p>
+      <ul v-if="!isLoading && results && results.length > 0">
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -21,9 +27,54 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
+  // props: ['results'],
   components: {
     SurveyResult,
+  },
+
+  data() {
+    return {
+      results: [],
+      isLoading: false,
+    };
+  },
+
+  methods: {
+    async loadExperiences() {
+      this.isLoading = true;
+      const data = await fetch(
+        'https://vue-http-demo-a17da-default-rtdb.europe-west1.firebasedatabase.app/surveys.json'
+      );
+
+      try {
+        const response = await data.json();
+
+        const loaded = await false;
+
+        this.isLoading = loaded;
+
+        console.log(response);
+
+        const results = [];
+
+        for (const id in response) {
+          results.push({
+            id: id,
+            name: response[id].name,
+            rating: response[id].rating,
+          });
+        }
+
+        this.results = results;
+
+        console.log(this.results);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+  mounted() {
+    this.loadExperiences();
   },
 };
 </script>
